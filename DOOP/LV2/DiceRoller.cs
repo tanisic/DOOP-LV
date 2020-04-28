68167585e838c42b8ef070659ac7203da5545733
tanisic98 <64195347+tanisic98@users.noreply.github.com>
2020-04-28 16:01:44 +0200

    LV2 prvi zadatak

---
+++ DOOP/LV2/DiceRoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LV2
+{
+    class DiceRoller
+    {
+        private List<Die> dice;
+        private List<int> resultForEachRoll;
+
+        public DiceRoller()
+        {
+            this.dice = new List<Die>();
+            this.resultForEachRoll = new List<int>();
+        }
+        public void InsertDie(Die die)
+        {
+            dice.Add(die);
+        }
+        public void RollAllDice()
+        {
+
+            this.resultForEachRoll.Clear();
+            foreach (Die die in dice)
+            {
+                this.resultForEachRoll.Add(die.Roll());
+            }
+        }
+
+        public IList<int> GetRollingResults()
+        {
+            return new System.Collections.ObjectModel.ReadOnlyCollection<int>(
+           this.resultForEachRoll
+           );
+        }
+        public int DiceCount
+        {
+            get { return dice.Count; }
+        }
+    }
+}
