5399fff6ecf20306caf23ad12ffe7acadcc12c7b
tanisic98 <64195347+tanisic98@users.noreply.github.com>
2020-04-29 11:00:42 +0200

    LV2 1.zadatak

---
+++ DOOP/LV2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //1.zadatak
+            DiceRoller diceRoller = new DiceRoller();
+
+            for(int i=0;i<20;i++)
+            {
+                diceRoller.InsertDie(new Die(6));
+            }
+            diceRoller.RollAllDice();
+            foreach(int number in diceRoller.GetRollingResults())
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
