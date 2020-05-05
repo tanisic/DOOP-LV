69094cea5aa761e48890f8673bfd208bb4af2667
tanisic98 <tin.anisic@hotmail.com>
2020-05-05 20:33:53 +0200

    LV3 7.zadatak

--- DOOP/LV3/ConsoleNotification.cs
+++ DOOP/LV3/ConsoleNotification.cs
@@ -7,7 +7,7 @@ namespace LV3
     public enum Category { ERROR, ALERT, INFO }
 
 
-    class ConsoleNotification
+    class ConsoleNotification:Prototype
     {
         public String Author { get; private set; }
         public String Title { get; private set; }
@@ -24,6 +24,14 @@ namespace LV3
             this.Timestamp = time;
             this.Level = level;
             this.Color = color;
+        }
+        /*7. zadatak  Odgovor: U konkretnom slučaju mislim da ima razlike između deep i shallow kopije
+         * zbog toga što sa shallow kopijom i dalje možemo mjenjati vrijednosti originala i kopije,
+         * dok kod deep kopije radimo novi objekt i nezavisan je od svog originala.
+        */
+        public Prototype Clone()
+        {
+            return (ConsoleNotification)this.MemberwiseClone();
         }
     }
 }
--- DOOP/LV3/Program.cs
+++ DOOP/LV3/Program.cs
@@ -5,18 +5,10 @@ using System.Globalization;
 namespace LV3
 {
     class Program
-    {   //6. zadatak
+    {   //7. zadatak
         static void Main(string[] args)
         {
-            NotificationBuilder builder = new NotificationBuilder();
-            Direktor direktor = new Direktor();
-            NotificationManager manager = new NotificationManager();
-            direktor.ConstructConsoleNotificationALERT(builder, "Ante");
-            manager.Display(builder.Build());
-            direktor.ConstructConsoleNotificationINFO(builder, "Ivan");
-            manager.Display(builder.Build());
-            direktor.ConstructConsoleNotificationERROR(builder, "Marin");
-            manager.Display(builder.Build());
+           
         }
     }
 }
