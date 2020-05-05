e2102d7159bd484b9843caf5f478a7bb4a9cda53
tanisic98 <tin.anisic@hotmail.com>
2020-05-05 18:49:45 +0200

    LV3 3. zadatak

---
+++ DOOP/LV3/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LV3
+{
+    class Logger
+    {
+        private static Logger thisInstance;
+        private string filePath;
+        public Logger(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public static Logger GetInstance(string filePath)
+        {
+            if(thisInstance==null)
+            {
+                thisInstance = new Logger(filePath);
+            }
+            return thisInstance;
+        }
+        public void Log(string message)
+        {
+            using (StreamWriter stream = new StreamWriter(filePath, true))
+            {
+                stream.WriteLine(message);
+            }
+
+        }
+    }
+}
--- DOOP/LV3/Program.cs
+++ DOOP/LV3/Program.cs
@@ -5,19 +5,12 @@ using System.Globalization;
 namespace LV3
 {
     class Program
-    {   //2. zadatak
+    {   //3. zadatak
         static void Main(string[] args)
         {
-            MatrixGenerator matrixGenerator = MatrixGenerator.GetInstance();
-            double[,] matrix = matrixGenerator.GenerateMatrix(7, 7);
-             for(int i=0;i<7;i++)
-            {
-                for(int j=0;j<7;j++)
-                {
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.WriteLine();
-            }    
+            Logger logger = Logger.GetInstance(@"C:\Users\tin_k\Documents\DOOP-LV\DOOP\LV3\LoggerOutput.txt");
+            string message = "Poruka za log file.";
+            logger.Log(message);
         }
     }
 }
