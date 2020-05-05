04b2d8e2b6168b78f0e94526ff4665e3c0074186
tanisic98 <tin.anisic@hotmail.com>
2020-05-05 18:29:48 +0200

    LV3 2.zadatak

---
+++ DOOP/LV3/MatrixGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Globalization;
+using System.Text;
+
+namespace LV3
+{
+    class MatrixGenerator
+    {
+        private static MatrixGenerator thisInstance;
+        private Random random;
+        
+        private MatrixGenerator()
+        {
+            this.random = new Random();
+        }
+        public static MatrixGenerator GetInstance()
+        {
+            if(thisInstance==null)
+            {
+                thisInstance = new MatrixGenerator();
+            }
+            return thisInstance;
+        }
+        public double[,] GenerateMatrix(int rows,int columns)
+        {
+            double[,] matrix = new double[rows,columns];
+            for(int i=0;i<rows;i++)
+            {   for(int j=0;j<columns;j++)
+                {
+                    matrix[i,j] = random.NextDouble();
+                }
+            }
+            return matrix;
+        }
+
+    }
+}
--- DOOP/LV3/Program.cs
+++ DOOP/LV3/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Data;
+using System.Globalization;
 
 namespace LV3
 {
     class Program
-    {   //1. zadatak
+    {   //2. zadatak
         static void Main(string[] args)
-        {   //duboko kloniranje
-            Dataset datasetFile = new Dataset();
-            datasetFile.LoadDataFromCSV(@"C:\Users\tin_k\Documents\DOOP-LV\DOOP\LV3\csvfile.csv");
-            Dataset deepCopyData = (Dataset)datasetFile.Clone();          
-            datasetFile.printDataset();
-            deepCopyData.printDataset();
-                       
+        {
+            MatrixGenerator matrixGenerator = MatrixGenerator.GetInstance();
+            double[,] matrix = matrixGenerator.GenerateMatrix(7, 7);
+             for(int i=0;i<7;i++)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }    
         }
     }
 }
