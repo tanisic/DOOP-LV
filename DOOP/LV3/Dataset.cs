a9c9060aa470eff9e78ea2b8a1febabdb18cc5dd
tanisic98 <tin.anisic@hotmail.com>
2020-05-05 18:09:41 +0200

    LV3

---
+++ DOOP/LV3/Dataset.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    class Dataset:Prototype
+    {
+        private List<List<string>> data; //list of lists of strings
+        public Dataset()
+        {
+            this.data = new List<List<string>>();
+        }
+        public void LoadDataFromCSV(string filePath)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<string> row = new List<string>();
+                    string[] items = line.Split(',');
+                    foreach (string item in items)
+                    {
+                        row.Add(item);
+                    }
+                    this.data.Add(row);
+                }
+            }
+        }
+        public Dataset(string filePath) : this()
+        {
+            this.LoadDataFromCSV(filePath);
+        }
+        
+        public Prototype Clone()
+        {
+            Dataset other = new Dataset();
+            var list = new List<List<string>>();
+            foreach (List<string> secondList in this.data)
+            {
+                var secondStringList = new List<string>();
+                foreach (string item in secondList)
+                {
+                    var clone = (string)item.Clone();
+                    secondStringList.Add(clone);
+                }
+                list.Add(secondStringList);
+            }
+            other.data = list;
+            return other;
+        }
+       
+        public IList<List<string>> GetData()
+        {
+            return new System.Collections.ObjectModel.ReadOnlyCollection<List<string>>(data);
+        }
+        public void ClearData()
+        {
+            this.data.Clear();
+        }
+        public void printDataset()
+        {
+            foreach(List<string> secondList in this.data)
+            {
+                foreach(string item in secondList)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ DOOP/LV3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LV3
+{
+    class Program
+    {   //1. zadatak
+        static void Main(string[] args)
+        {   //duboko kloniranje
+            Dataset datasetFile = new Dataset();
+            datasetFile.LoadDataFromCSV(@"C:\Users\tin_k\Documents\DOOP-LV\DOOP\LV3\csvfile.csv");
+            Dataset deepCopyData = (Dataset)datasetFile.Clone();          
+            datasetFile.printDataset();
+            deepCopyData.printDataset();
+                       
+        }
+    }
+}
