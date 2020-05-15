a58017984d0a70751907933790da8d4efd46e77b
tanisic98 <tin.anisic@hotmail.com>
2020-05-15 12:30:34 +0200

    LV5 1.zadatak

---
+++ DOOP/LV5/Box.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class Box : IShipable
+    {
+        private List<IShipable> items;
+        private string name;
+        private double weight;
+        public Box(string name)
+        {
+            this.items = new List<IShipable>();
+            this.name = name;
+        }
+        public void Add(IShipable item)
+        {
+            this.items.Add(item);
+        }
+        public void Remove(IShipable item)
+        {
+            this.items.Remove(item);
+        }
+        public double Price
+        {
+            get
+            {
+                double totalPrice = 0;
+                foreach (IShipable item in items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                double totalWeight = 0;
+                foreach (IShipable item in items)
+                {
+                    totalWeight += item.Weight;
+                }
+                return totalWeight;
+            }
+        }
+        public string Description(int depth = 0)
+        {
+            StringBuilder builder =
+           new StringBuilder(new string(' ', depth) + this.name + "\n");
+            foreach (IShipable item in items)
+            {
+                builder.Append(item.Description(depth + 2)).Append("\n");
+            }
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ DOOP/LV5/IShipable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    interface IShipable
+    {
+        double Price { get; }
+        string Description(int depth = 0);
+        double Weight { get; }
+    }
+}
