a3b043482510f38c968b54ea07a4a8443ad199d3
tanisic98 <tin.anisic@hotmail.com>
2020-05-15 14:01:50 +0200

    LV5 5.zadatak

---
+++ DOOP/LV5/GroupNote.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class GroupNote : Note
+    {
+        private List<string> names;
+        public GroupNote(string message, ITheme theme) : base(message, theme)
+        {
+            this.names = new List<string>();
+        }
+        public void PrintNames()
+        {
+           foreach(string name in names)
+            {
+                Console.Write(name + ", ");
+            }
+            Console.WriteLine();
+        }
+        public void AddName(string name)
+        {
+            names.Add(name);
+        }
+        public void RemoveName(string name)
+        {
+            if(names.Contains(name))
+            {
+                names.Remove(name);
+            }
+        }
+
+        public override void Show()
+        {
+            this.ChangeColor();
+            Console.WriteLine("REMINDER: ");   
+            string framedMessage = this.GetFramedMessage();
+            this.PrintNames();
+            Console.WriteLine(framedMessage);
+            Console.ResetColor();
+        }
+    }
+}
--- DOOP/LV5/Program.cs
+++ DOOP/LV5/Program.cs
@@ -7,9 +7,17 @@ namespace LV5
     class Program
     {
         static void Main(string[] args)
-        { //4. zadatak
-            ReminderNote reminder = new ReminderNote("Vazna poruka", new BlueTheme());
-            reminder.Show();
+        { //5. zadatak
+           
+            GroupNote groupNote1 = new GroupNote("Grupna poruka 1",new BlueTheme());
+            groupNote1.AddName("Mario");
+            groupNote1.AddName("Ante");
+            groupNote1.AddName("Petar");
+            groupNote1.Show();
+            GroupNote groupNote2 = new GroupNote("Grupna poruka 2", new LightTheme());
+            groupNote2.AddName("Martin");
+            groupNote2.AddName("Marijan");
+            groupNote2.Show();
         }
     }
 }
