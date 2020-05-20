db2138bd2fbed392b382f0dad208c1bb7a6d37cf
tanisic98 <tin.anisic@hotmail.com>
2020-05-20 12:06:35 +0200

    LV6 3.zadatak

---
+++ DOOP/LV6/CareTaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV6
+{
+    class CareTaker
+    {
+        public List<Memento> PreviousStates { get; set; }
+        public CareTaker()
+        {
+            this.PreviousStates = new List<Memento>();
+        }
+        public void AddMemento(Memento memento)
+        {
+            PreviousStates.Add(memento);
+        }
+        public void RemoveMemento(Memento memento)
+        {
+            PreviousStates.Remove(memento);
+        }
+        public List<Memento> GetMementoList()
+        {
+            return this.PreviousStates;
+        }
+    }
+}
--- DOOP/LV6/Program.cs
+++ DOOP/LV6/Program.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 namespace LV6
 {
     class Program
-    { //2. zadatak
+    { //3. zadatak
         static void Main(string[] args)
         {
-            List<Product> products = new List<Product>();
-            for(int i=0;i<3;i++)
+            ToDoItem item1 = new ToDoItem("title1", "text1", DateTime.Today);
+            CareTaker careTaker = new CareTaker();
+            careTaker.AddMemento(item1.StoreState());
+            item1 = new ToDoItem("title2", "text2", DateTime.Today);
+            careTaker.AddMemento(item1.StoreState());
+            item1 = new ToDoItem("title3","text3",DateTime.Today);
+            careTaker.AddMemento(item1.StoreState());
+            foreach(Memento memento in careTaker.GetMementoList())
             {
-                products.Add(new Product("description",72.05));
-            }
-            Box box = new Box(products);
-            ProductIterator iterator = (ProductIterator)box.GetIterator();
-            while(!iterator.IsDone)
-            {
-                Console.WriteLine(iterator.Current.ToString());
-                iterator.Next();
+                item1.RestoreState(memento);
+                Console.WriteLine(item1.ToString());
             }
         }
     }
