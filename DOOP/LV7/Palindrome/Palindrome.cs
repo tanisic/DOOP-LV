c3def4bd7fc05d2f63c5fdb053e2911af837eceb
tanisic98 <tin.anisic@hotmail.com>
2020-05-29 14:17:59 +0200

    LV7 1. zadatak

---
+++ DOOP/LV7/Palindrome/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palindrome
+    { //1. zadatak
+        public string ConvertToPalindrome(string value)
+        {
+            string filteredString = value.ToLower();
+            char[] array = filteredString.ToCharArray();
+            for(int i = 0; i< array.Length;i++)
+            {
+                if(!char.IsLetterOrDigit(array[i]))
+                {
+                    array[i] = ' ';
+                }
+            }              
+            Array.Reverse(array);         
+            return new string(array).Replace(" ","");
+        }
+    }
+}
